3f1b5c39882a2b74c923233180727164a4f6b3ae
HendraSiskom <hendra10@student.untan.ac.id>
2022-12-21 15:29:44 +0700

    fix aktif/batal

--- resources/views/penatausahaan/spd/pembatalan_spd/index.blade.php
+++ resources/views/penatausahaan/spd/pembatalan_spd/index.blade.php
@@ -1,11 +1,11 @@
 @extends('template.app')
-@section('title', 'PEMBATALAN SPD | SIMAKDA')
+@section('title', 'Aktif/Batal SPD | SIMAKDA')
 @section('content')
 <div class="row">
     <div class="col-12">
         <div class="card">
             <div class="card-header">
-                <h5 style="font-weight: bold;">PEMBATALAN SPD</h5>
+                <h5 style="font-weight: bold;">Aktif/Batal SPD</h5>
             </div>
             <div class="card-body">
                 <div class="table-rep-plugin">
