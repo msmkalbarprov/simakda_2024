d892e366c05e54fba59ac7a97a238827a4fd72c2
msmkalbarprov <msmkalbar.prov@gmail.com>
2023-01-13 09:53:40 +0700

    update penerimaan

--- resources/views/skpd/laporan_bendahara_penerimaan/index.blade.php
+++ resources/views/skpd/laporan_bendahara_penerimaan/index.blade.php
@@ -22,7 +22,7 @@
         <div class="col-md-6">
             <div class="card card-info collapsed-card card-outline" id="lapbku">
                 <div class="card-body">
-                    {{ 'Buku Penerimaan dan Pengeluaran' }}
+                    {{ 'Buku Penerimaan dan Penyetoran' }}
                     <a class="card-block stretched-link" href="#">
 
                     </a>
