7ad6c67a880647f41e6dee3fd8e1c9b5a68222d1
daniels9028 <daniels9028@student.untan.ac.id>
2023-01-27 09:28:31 +0700

    permintaan spp

--- resources/views/penatausahaan/pengeluaran/spp_ls/cetak/permintaan.blade.php
+++ resources/views/penatausahaan/pengeluaran/spp_ls/cetak/permintaan.blade.php
@@ -16,14 +16,7 @@
 </head>
 
 <body>
-    {{-- <div style="text-align: left;margin-top:20px">
-        <h5 style="margin: 2px 0px">PEMERINTAH PROVINSI KALIMANTAN BARAT</h5>
-        <h5 style="margin: 2px 0px">{{ $nama_skpd }}</h5>
-        <h5 style="margin: 2px 0px">TAHUN ANGGARAN {{ $tahun_anggaran }}</h5>
-        <div style="clear: both"></div>
-    </div>
-    <hr> --}}
-    <table style="border-collapse:collapse;font-family: Open Sans; font-size:12px" width="100%" align="center"
+    {{-- <table style="border-collapse:collapse;font-family: Open Sans; font-size:12px" width="100%" align="center"
         border="0" cellspacing="0" cellpadding="0">
         <tr>
             <td rowspan="5" align="left" width="7%">
@@ -45,162 +38,181 @@
         <tr>
             <td align="left" style="font-size:14px"><strong>&nbsp;</strong></td>
         </tr>
-    </table>
+    </table> --}}
     <hr>
-    <div style="text-align: center">
-        <h5 style="margin: 2px 0px">SURAT PERMINTAAN PEMBAYARAN {{ strtoupper($lcbeban) }}</h5>
-        <h5 style="margin: 2px 0px">(SPP - LS {{ strtoupper($lcbeban) }})</h5>
-        <h5 style="margin: 2px 0px">Nomor : {{ $no_spp }}</h5>
-    </div>
+    <table style="width: 100%;text-align:center;font-family:'Times New Roman', Times, serif">
+        <tr>
+            <td><b>SURAT PERMINTAAN PEMBAYARAN {{ strtoupper($lcbeban) }}</b></td>
+        </tr>
+        <tr>
+            <td><b>(SPP - LS {{ strtoupper($lcbeban) }})</b></td>
+        </tr>
+        <tr>
+            <td><b>Nomor : {{ $no_spp }}</b></td>
+        </tr>
+    </table>
+    <br>
 
-    <div style="text-align: left">
-        <table class="table" style="width: 100%;border:1px black solid;font-family: Open Sans; font-size:12px">
-            {{-- OPD --}}
-            <tr>
-                <td width='30%'>1. OPD</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ $nama_skpd }}</td>
-            </tr>
-            {{-- Unit Kerja --}}
-            <tr>
-                <td width='30%'>2. Unit Kerja</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ $nama_skpd }}</td>
-            </tr>
-            {{-- Alamat --}}
-            <tr>
-                <td width='30%'>3. Alamat</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ $alamat_skpd }}</td>
-            </tr>
-            {{-- Nomor dan Tanggal DPA/DPPA/DPPAL-OPD --}}
-            <tr>
-                <td width='30%'>4. Nomor dan Tanggal DPA/DPPA/DPPAL-OPD</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ $dpa->no_dpa }} / {{ $dpa->tgl_dpa }}</td>
-            </tr>
-            {{-- Tahun Anggaran --}}
-            <tr>
-                <td width='30%'>5. Tahun Anggaran</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ $tahun_anggaran }}</td>
-            </tr>
-            {{-- Bulan --}}
-            <tr>
-                <td width='30%'>6. Bulan</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ bulan($data_spp->bulan) }} {{ $tahun_anggaran }}</td>
-            </tr>
-            {{-- Urusan Pemerintahan --}}
-            <tr>
-                <td width='30%'>7. Urusan Pemerintahan</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ $data_spp->kd_bidang_urusan }} {{ $data_spp->nm_bidang_urusan }}</td>
-            </tr>
-            {{-- Nama Program --}}
-            <tr>
-                <td width='30%'>8. Nama Program</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ $nama_program }}</td>
-            </tr>
-            {{-- Nama Kegiatan --}}
-            <tr>
-                <td width='30%'>9. Nama Kegiatan</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ $nama_kegiatan }}</td>
-            </tr>
-            {{-- Nama Sub Kegiatan --}}
-            <tr>
-                <td width='30%'>10. Nama Sub Kegiatan</td>
-                <td width='2%'>:</td>
-                <td width='68%'>{{ $data_spp->nm_sub_kegiatan }}</td>
-            </tr>
-        </table>
-    </div>
+    <table class="table"
+        style="width: 100%;border:1px black solid;font-family:'Times New Roman', Times, serif; font-size:14px">
+        {{-- OPD --}}
+        <tr>
+            <td width='30%'>1. OPD</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $nama_skpd }}</td>
+        </tr>
+        {{-- Unit Kerja --}}
+        <tr>
+            <td width='30%'>2. Unit Kerja</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $nama_skpd }}</td>
+        </tr>
+        {{-- Alamat --}}
+        <tr>
+            <td width='30%'>3. Alamat</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $alamat_skpd }}</td>
+        </tr>
+        {{-- Nomor dan Tanggal DPA/DPPA/DPPAL-OPD --}}
+        <tr>
+            <td width='30%'>4. Nomor dan Tanggal DPA/DPPA/DPPAL-OPD</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $dpa->no_dpa }} / {{ $dpa->tgl_dpa }}</td>
+        </tr>
+        {{-- Tahun Anggaran --}}
+        <tr>
+            <td width='30%'>5. Tahun Anggaran</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $tahun_anggaran }}</td>
+        </tr>
+        {{-- Bulan --}}
+        <tr>
+            <td width='30%'>6. Bulan</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ bulan($data_spp->bulan) }} {{ $tahun_anggaran }}</td>
+        </tr>
+        {{-- Urusan Pemerintahan --}}
+        <tr>
+            <td width='30%'>7. Urusan Pemerintahan</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $data_spp->kd_bidang_urusan }} {{ $data_spp->nm_bidang_urusan }}</td>
+        </tr>
+        {{-- Nama Program --}}
+        <tr>
+            <td width='30%'>8. Nama Program</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $nama_program }}</td>
+        </tr>
+        {{-- Nama Kegiatan --}}
+        <tr>
+            <td width='30%'>9. Nama Kegiatan</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $nama_kegiatan }}</td>
+        </tr>
+        {{-- Nama Sub Kegiatan --}}
+        <tr>
+            <td width='30%'>10. Nama Sub Kegiatan</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $data_spp->nm_sub_kegiatan }}</td>
+        </tr>
+    </table>
+    <br>
 
-    <div style="text-align: center">
-        <h5 style="margin:2px 0px">Kepada Yth:</h5>
-        <h5 style="margin:2px 0px">Pengguna Anggaran/Kuasa Pengguna Anggaran</h5>
-        <h5 style="margin:2px 0px">{{ $nama_skpd }}</h5>
-        <h5 style="margin:2px 0px">di {{ $daerah->daerah }}</h5>
-    </div>
+    <table style="width: 100%;text-align:center;font-family:'Times New Roman', Times, serif">
+        <tr>
+            <td><b>Kepada Yth:</b></td>
+        </tr>
+        <tr>
+            <td><b>Pengguna Anggaran/Kuasa Pengguna Anggaran</b></td>
+        </tr>
+        <tr>
+            <td><b>{{ $nama_skpd }}</b></td>
+        </tr>
+        <tr>
+            <td><b>di {{ $daerah->daerah }}</b></td>
+        </tr>
+    </table>
 
-    <div style="text-align: left">
-        <h5 style="margin: 2px 0px">Dengan memperhatikan Peraturan Gubernur Kalimantan Barat {{ $nogub }}
-            tentang Penjabaran APBD Tahun
-            Anggaran {{ $tahun_anggaran }}, bersama ini kami mengajukan Surat Permintaan Pembayaran Langsung Barang dan
-            Jasa sebagai berikut:</h5>
-    </div>
+    <table style="width: 100%;font-family:'Times New Roman', Times, serif">
+        <tr>
+            <td style="text-align: justify">Dengan memperhatikan Peraturan Gubernur Kalimantan Barat
+                {{ $nogub }}
+                tentang Penjabaran APBD Tahun
+                Anggaran {{ $tahun_anggaran }}, bersama ini kami mengajukan Surat Permintaan Pembayaran Langsung Barang
+                dan
+                Jasa sebagai berikut:</td>
+        </tr>
+    </table>
 
-    <div style="text-align: left">
-        <table class="table" style="width: 100%;border:1px black solid;font-family: Open Sans; font-size:12px">
-            {{-- Jumlah Pembayaran Yang Diminta --}}
+    <table class="table"
+        style="width: 100%;border:1px black solid;font-family:'Times New Roman', Times, serif; font-size:14px">
+        {{-- Jumlah Pembayaran Yang Diminta --}}
+        <tr>
+            <td width='30%'>a. Jumlah Pembayaran Yang Diminta</td>
+            <td width='2%'>:</td>
+            <td width='68%'>Rp.{{ rupiah($data_spp->nilai) }}</td>
+        </tr>
+        {{-- Terbilang --}}
+        <tr>
+            <td style="width:400px;text-align:center">(terbilang)</td>
+            <td>:</td>
+            <td style="font-style: italic">{{ ucwords(terbilang($data_spp->nilai)) }}</td>
+        </tr>
+        {{-- Untuk Keperluan --}}
+        <tr>
+            <td width='30%'>b. Untuk Keperluan</td>
+            <td width='2%'>:</td>
+            <td width='68%'>{{ $data_spp->keperluan }}</td>
+        </tr>
+        {{-- Nama Bendahara --}}
+        @if ($beban == '6' && $jenis == '6')
             <tr>
-                <td width='30%'>a. Jumlah Pembayaran Yang Diminta</td>
+                <td width='30%'>c. Nama Pihak Ketiga</td>
                 <td width='2%'>:</td>
-                <td width='68%'>Rp.{{ rupiah($data_spp->nilai) }}</td>
+                <td width='68%'>{{ $data_spp->nmrekan }}</td>
             </tr>
-            {{-- Terbilang --}}
             <tr>
-                <td style="width:400px;text-align:center">(terbilang)</td>
-                <td>:</td>
-                <td style="font-style: italic">{{ ucwords(terbilang($data_spp->nilai)) }}</td>
-            </tr>
-            {{-- Untuk Keperluan --}}
-            <tr>
-                <td width='30%'>b. Untuk Keperluan</td>
+                <td width='30%'>d. Dasar Bendahara Pengeluaran</td>
                 <td width='2%'>:</td>
-                <td width='68%'>{{ $data_spp->keperluan }}</td>
+                <td width='68%'>{{ $no_spd }}</td>
             </tr>
-            {{-- Nama Bendahara --}}
-            @if ($beban == '6' && $jenis == '6')
-                <tr>
-                    <td width='30%'>c. Nama Pihak Ketiga</td>
-                    <td width='2%'>:</td>
-                    <td width='68%'>{{ $data_spp->nmrekan }}</td>
-                </tr>
-                <tr>
-                    <td width='30%'>d. Dasar Bendahara Pengeluaran</td>
-                    <td width='2%'>:</td>
-                    <td width='68%'>{{ $no_spd }}</td>
-                </tr>
-                <tr>
-                    <td width='30%'>e. Alamat</td>
-                    <td width='2%'>:</td>
-                    <td width='68%'>{{ $data_spp->alamat }}</td>
-                </tr>
-                <tr>
-                    <td width='30%'>f. Nama dan Nomor Rekening</td>
-                    <td width='2%'>:</td>
-                    <td width='68%'>{{ $data_spp->nama_bank_rek }} / {{ $data_spp->no_rek_rek }}</td>
-                </tr>
-            @else
-                <tr>
-                    <td width='30%'>c. Nama Bendahara</td>
-                    <td width='2%'>:</td>
-                    <td width='68%'>{{ $cari_bendahara->nama }}</td>
-                </tr>
-                <tr>
-                    <td width='30%'>d. Dasar Bendahara Pengeluaran</td>
-                    <td width='2%'>:</td>
-                    <td width='68%'>{{ $no_spd }}</td>
-                </tr>
-                <tr>
-                    <td width='30%'>e. Alamat</td>
-                    <td width='2%'>:</td>
-                    <td width='68%'>{{ $data_spp->alamat }}</td>
-                </tr>
-                <tr>
-                    <td width='30%'>f. Nama dan Nomor Rekening</td>
-                    <td width='2%'>:</td>
-                    <td width='68%'>{{ $nama_bank->nama }} / {{ $data_spp->no_rek }}</td>
-                </tr>
-            @endif
-        </table>
-    </div>
+            <tr>
+                <td width='30%'>e. Alamat</td>
+                <td width='2%'>:</td>
+                <td width='68%'>{{ $data_spp->alamat }}</td>
+            </tr>
+            <tr>
+                <td width='30%'>f. Nama dan Nomor Rekening</td>
+                <td width='2%'>:</td>
+                <td width='68%'>{{ $data_spp->nama_bank_rek }} / {{ $data_spp->no_rek_rek }}</td>
+            </tr>
+        @else
+            <tr>
+                <td width='30%'>c. Nama Bendahara</td>
+                <td width='2%'>:</td>
+                <td width='68%'>{{ $cari_bendahara->nama }}</td>
+            </tr>
+            <tr>
+                <td width='30%'>d. Dasar Bendahara Pengeluaran</td>
+                <td width='2%'>:</td>
+                <td width='68%'>{{ $no_spd }}</td>
+            </tr>
+            <tr>
+                <td width='30%'>e. Alamat</td>
+                <td width='2%'>:</td>
+                <td width='68%'>{{ $data_spp->alamat }}</td>
+            </tr>
+            <tr>
+                <td width='30%'>f. Nama dan Nomor Rekening</td>
+                <td width='2%'>:</td>
+                <td width='68%'>{{ $nama_bank->nama }} / {{ $data_spp->no_rek }}</td>
+            </tr>
+        @endif
+    </table>
+
     {{-- tanda tangan --}}
     <div style="padding-top:20px">
-        <table class="table" style="width: 100%;font-family: Open Sans; font-size:12px">
+        <table class="table" style="width: 100%;font-family:'Times New Roman', Times, serif; font-size:14px">
             @if ($beban == '4' || $sub_kegiatan == '5.02.00.0.06.62')
                 <tr>
                     <td style="margin: 2px 0px;text-align: center;padding-left:500px">
