18aa29f9e72ffc0c2546740800c9091010334754
daniels9028 <daniels9028@student.untan.ac.id>
2023-01-27 19:46:06 +0700

    ringkasan

--- resources/views/penatausahaan/pengeluaran/spm/cetak/ringkasan_up.blade.php
+++ resources/views/penatausahaan/pengeluaran/spm/cetak/ringkasan_up.blade.php
@@ -59,8 +59,9 @@
     <br>
     <table style="width: 100%;font-family:'Open Sans', Helvetica,Arial,sans-serif;" class="rincian">
         <tr>
-            <td>Berdasarkan Keputusan Gubernur Nomor {{ $pergub->no_pergub }} Tanggal {{ tanggal($pergub->tgl_pergub) }}
-                Tentang {{ $pergub->tentang }} untuk SKPD {{ $data_beban->nm_skpd }} <br> sejumlah Rp
+            <td style="text-indent: 20px">Berdasarkan Keputusan Gubernur Nomor {{ $pergub->no_pergub }} Tanggal
+                {{ tanggal($pergub->tgl_pergub) }}
+                Tentang {{ $pergub->tentang }} untuk SKPD {{ $data_beban->nm_skpd }} sejumlah Rp
                 {{ rupiah($data_beban->nilai) }}</td>
         </tr>
         <tr>
