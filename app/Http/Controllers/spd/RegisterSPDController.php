5498224a97b54283f8e557d333f9f1adb83ecf6e
daniels9028 <daniels9028@student.untan.ac.id>
2023-01-30 15:05:22 +0700

    register spd

--- app/Http/Controllers/spd/RegisterSPDController.php
+++ app/Http/Controllers/spd/RegisterSPDController.php
@@ -10,42 +10,43 @@ use PDF;
 
 class RegisterSPDController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('penatausahaan.spd.register_spd.index');
     }
-    
+
     public function getSKPD(Request $request)
     {
         $term = $request->term;
 
-		$results = DB::table('ms_skpd')
+        $results = DB::table('ms_skpd')
             ->select('kd_skpd as id', 'nm_skpd as text', 'kd_skpd', 'nm_skpd')
-                ->when($term, function ($query, $term) {
-                    $query->where(function ($query) use ($term) {
-                        $query->orWhere('kd_skpd', 'like', '%' . $term . '%')
-                            ->orWhere('nm_skpd', 'like', '%' . $term . '%');
-                    });
-                })->get();
+            ->when($term, function ($query, $term) {
+                $query->where(function ($query) use ($term) {
+                    $query->orWhere('kd_skpd', 'like', '%' . $term . '%')
+                        ->orWhere('nm_skpd', 'like', '%' . $term . '%');
+                });
+            })->get();
 
-		return response()->json(['results' => $results]);
+        return response()->json(['results' => $results]);
     }
 
     public function getNipSKPD(Request $request)
     {
         $term = $request->term;
-		$kd_skpd = $request->kd_skpd;
+        $kd_skpd = $request->kd_skpd;
 
         $results = DB::table('ms_ttd')
             ->select('nip as id', 'nama as text', 'nip', 'nama', 'jabatan', 'kd_skpd')
-                ->whereIn('kode', ['PA', 'KPA'] )->where('kd_skpd', $kd_skpd)
-                ->when($term, function ($query, $term) {
-                    $query->where(function ($query) use ($term) {
-                        $query->orWhere('nip', 'like', '%' . $term . '%')
-                            ->orWhere('nama', 'like', '%' . $term . '%');
-                    });
-                })->get();
+            ->whereIn('kode', ['PA', 'KPA'])->where('kd_skpd', $kd_skpd)
+            ->when($term, function ($query, $term) {
+                $query->where(function ($query) use ($term) {
+                    $query->orWhere('nip', 'like', '%' . $term . '%')
+                        ->orWhere('nama', 'like', '%' . $term . '%');
+                });
+            })->get();
 
-		return response()->json(['results' => $results]);
+        return response()->json(['results' => $results]);
     }
 
     public function CetakURS(Request $request)
@@ -60,19 +61,19 @@ class RegisterSPDController extends Controller
         $total = 0;
 
         $skpd = DB::table('ms_skpd')->select('kd_skpd', 'nm_skpd')
-                    ->where(['kd_skpd' => $kd_skpd])->first();
+            ->where(['kd_skpd' => $kd_skpd])->first();
 
         $ttd = DB::table('ms_ttd')->select('nip', 'nama', 'pangkat', 'jabatan')
-                ->where(['nip' => $nip])->first();
+            ->where(['nip' => $nip])->first();
 
         $data = DB::table('trdspd as a')->select('a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama', DB::raw('sum(a.nilai) as nilai'))
-                ->join('trhspd as b', function ($join){
-                    $join->on('a.no_spd', '=', 'b.no_spd');
-                })->join('ms_ttd as c', function ($join){
-                    $join->on('b.kd_bkeluar', '=', 'c.nip');
-                })->whereRaw("b.tgl_spd >= ? and b.tgl_spd <= ? and b.kd_skpd = ?", [$tgl_awal, $tgl_akhir, $kd_skpd])
-                    ->groupBy(['a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama'])
-                    ->orderBy('b.tgl_spd')->orderBy('a.no_spd')->orderBy('b.kd_skpd')->get();
+            ->join('trhspd as b', function ($join) {
+                $join->on('a.no_spd', '=', 'b.no_spd');
+            })->join('ms_ttd as c', function ($join) {
+                $join->on('b.kd_bkeluar', '=', 'c.nip');
+            })->whereRaw("b.tgl_spd >= ? and b.tgl_spd <= ? and b.kd_skpd = ?", [$tgl_awal, $tgl_akhir, $kd_skpd])
+            ->groupBy(['a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama'])
+            ->orderBy('b.tgl_spd')->orderBy('a.no_spd')->orderBy('b.kd_skpd')->get();
 
         $view = view('penatausahaan.spd.register_spd.cetak.cetakurs', array(
             'jenis' => $jenis,
@@ -84,15 +85,15 @@ class RegisterSPDController extends Controller
             'tgl_akhir' => $tgl_akhir,
             'data' => $data,
             'total' => $total,
-		));
+        ));
         if ($jenis == 'pdf') {
             $pdf = PDF::loadHtml($view)->setOrientation('landscape')->setPaper('a4');
             return $pdf->stream('laporan.pdf');
-        }else if ($request->jenis == 'excel') {
+        } else if ($request->jenis == 'excel') {
             header("Cache-Control: no-cache, no-store, must-revalidate");
             header("Content-Type: application/vnd.ms-excel");
             header("Content-Disposition: attachment; filename=laporan.xls");
-			return $view;
+            return $view;
         } else {
             return $view;
         }
@@ -111,19 +112,19 @@ class RegisterSPDController extends Controller
         $total = 0;
 
         $skpd = DB::table('ms_organisasi')->select('kd_org', 'nm_org')
-                    ->where(['kd_org' => $kd_skpd])->first();
-                
+            ->where(['kd_org' => $kd_skpd])->first();
+
         $ttd = DB::table('ms_ttd')->select('nip', 'nama', 'pangkat', 'jabatan')
-                ->where(['nip' => $nip])->first();
+            ->where(['nip' => $nip])->first();
 
         $data = DB::table('trdspd as a')->select('a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama', DB::raw('sum(a.nilai) as nilai'))
-                ->join('trhspd as b', function ($join){
-                    $join->on('a.no_spd', '=', 'b.no_spd');
-                })->join('ms_ttd as c', function ($join){
-                    $join->on('b.kd_bkeluar', '=', 'c.nip');
-                })->whereRaw("b.tgl_spd >= ? and b.tgl_spd <= ? and left(b.kd_skpd, 17) = ?", [$tgl_awal, $tgl_akhir, $kd_skpd])
-                    ->groupBy(['a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama'])
-                    ->orderBy('b.tgl_spd')->orderBy('a.no_spd')->orderBy('b.kd_skpd')->get();
+            ->join('trhspd as b', function ($join) {
+                $join->on('a.no_spd', '=', 'b.no_spd');
+            })->join('ms_ttd as c', function ($join) {
+                $join->on('b.kd_bkeluar', '=', 'c.nip');
+            })->whereRaw("b.tgl_spd >= ? and b.tgl_spd <= ? and left(b.kd_skpd, 17) = ?", [$tgl_awal, $tgl_akhir, $kd_skpd])
+            ->groupBy(['a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama'])
+            ->orderBy('b.tgl_spd')->orderBy('a.no_spd')->orderBy('b.kd_skpd')->get();
 
         $view = view('penatausahaan.spd.register_spd.cetak.cetaksrs', array(
             'jenis' => $jenis,
@@ -135,15 +136,15 @@ class RegisterSPDController extends Controller
             'tgl_akhir' => $tgl_akhir,
             'data' => $data,
             'total' => $total,
-		));
+        ));
         if ($jenis == 'pdf') {
             $pdf = PDF::loadHtml($view)->setOrientation('landscape')->setPaper('a4');
             return $pdf->stream('laporan.pdf');
-        }else if ($request->jenis == 'excel') {
+        } else if ($request->jenis == 'excel') {
             header("Cache-Control: no-cache, no-store, must-revalidate");
             header("Content-Type: application/vnd.ms-excel");
             header("Content-Disposition: attachment; filename=laporan.xls");
-			return $view;
+            return $view;
         } else {
             return $view;
         }
@@ -157,14 +158,14 @@ class RegisterSPDController extends Controller
 
         $total = 0;
 
-        $data = DB::table('trdspd as a')->select('a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama', DB::raw('sum(a.nilai) as nilai'))
-                ->join('trhspd as b', function ($join){
-                    $join->on('a.no_spd', '=', 'b.no_spd');
-                })->join('ms_ttd as c', function ($join){
-                    $join->on('b.kd_bkeluar', '=', 'c.nip');
-                })->whereRaw("b.tgl_spd >= ? and b.tgl_spd <= ?", [$tgl_awal, $tgl_akhir])
-                    ->groupBy(['a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama'])
-                    ->orderBy('b.tgl_spd')->orderBy('a.no_spd')->orderBy('b.kd_skpd')->get();
+        $data = DB::table('trdspd as a')
+            ->select('a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', DB::raw('sum(a.nilai) as nilai'))
+            ->selectRaw("(SELECT TOP 1 nama from ms_ttd where b.kd_bkeluar=nip) as nama")
+            ->join('trhspd as b', function ($join) {
+                $join->on('a.no_spd', '=', 'b.no_spd');
+            })->whereRaw("b.tgl_spd >= ? and b.tgl_spd <= ?", [$tgl_awal, $tgl_akhir])
+            ->groupBy(['a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'b.kd_bkeluar'])
+            ->orderBy('b.tgl_spd')->orderBy('a.no_spd')->orderBy('b.kd_skpd')->get();
 
         $view = view('penatausahaan.spd.register_spd.cetak.cetakkrs', array(
             'jenis' => $jenis,
@@ -172,15 +173,15 @@ class RegisterSPDController extends Controller
             'tgl_akhir' => $tgl_akhir,
             'data' => $data,
             'total' => $total,
-		));
+        ));
         if ($jenis == 'pdf') {
             $pdf = PDF::loadHtml($view)->setOrientation('landscape')->setPaper('a4');
             return $pdf->stream('laporan.pdf');
-        }else if ($request->jenis == 'excel') {
+        } else if ($request->jenis == 'excel') {
             header("Cache-Control: no-cache, no-store, must-revalidate");
             header("Content-Type: application/vnd.ms-excel");
             header("Content-Disposition: attachment; filename=laporan.xls");
-			return $view;
+            return $view;
         } else {
             return $view;
         }
@@ -194,19 +195,27 @@ class RegisterSPDController extends Controller
 
         $total = 0;
 
-        $data = DB::table('trdspd as a')->select('a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama', DB::raw('sum(a.nilai) as nilai'),
-                    'revisi_ke', DB::raw('(SELECT isnull(max(revisi_ke),0) as revisi from trhspd where kd_skpd=b.kd_skpd and bulan_akhir=b.bulan_akhir) as revisi'))
-                ->join('trhspd as b', function ($join){
-                    $join->on('a.no_spd', '=', 'b.no_spd');
-                })->join('ms_ttd as c', function ($join){
-                    $join->on('b.kd_bkeluar', '=', 'c.nip');
-                })->whereRaw("b.tgl_spd >= ? and b.tgl_spd <= ?", [$tgl_awal, $tgl_akhir])
-                    ->groupBy(['a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama', 'bulan_awal', 'bulan_akhir', 'revisi_ke']);
+        $data = DB::table('trdspd as a')->select(
+            'a.no_spd',
+            'b.tgl_spd',
+            'b.kd_skpd',
+            'b.nm_skpd',
+            'c.nama',
+            DB::raw('sum(a.nilai) as nilai'),
+            'revisi_ke',
+            DB::raw('(SELECT isnull(max(revisi_ke),0) as revisi from trhspd where kd_skpd=b.kd_skpd and bulan_akhir=b.bulan_akhir) as revisi')
+        )
+            ->join('trhspd as b', function ($join) {
+                $join->on('a.no_spd', '=', 'b.no_spd');
+            })->join('ms_ttd as c', function ($join) {
+                $join->on('b.kd_bkeluar', '=', 'c.nip');
+            })->whereRaw("b.tgl_spd >= ? and b.tgl_spd <= ?", [$tgl_awal, $tgl_akhir])
+            ->groupBy(['a.no_spd', 'b.tgl_spd', 'b.kd_skpd', 'b.nm_skpd', 'c.nama', 'bulan_awal', 'bulan_akhir', 'revisi_ke']);
 
         $data1 = DB::table(DB::raw("({$data->toSql()}) AS sub"))
-                ->mergeBindings($data)
-                ->whereRaw("revisi_ke = revisi")
-                ->orderBy('tgl_spd')->orderBy('no_spd')->orderBy('kd_skpd')->get();
+            ->mergeBindings($data)
+            ->whereRaw("revisi_ke = revisi")
+            ->orderBy('tgl_spd')->orderBy('no_spd')->orderBy('kd_skpd')->get();
 
         $view = view('penatausahaan.spd.register_spd.cetak.cetakkrrs', array(
             'jenis' => $jenis,
@@ -214,15 +223,15 @@ class RegisterSPDController extends Controller
             'tgl_akhir' => $tgl_akhir,
             'data1' => $data1,
             'total' => $total,
-		));
+        ));
         if ($jenis == 'pdf') {
             $pdf = PDF::loadHtml($view)->setOrientation('landscape')->setPaper('a4');
             return $pdf->stream('laporan.pdf');
-        }else if ($request->jenis == 'excel') {
+        } else if ($request->jenis == 'excel') {
             header("Cache-Control: no-cache, no-store, must-revalidate");
             header("Content-Type: application/vnd.ms-excel");
             header("Content-Disposition: attachment; filename=laporan.xls");
-			return $view;
+            return $view;
         } else {
             return $view;
         }
