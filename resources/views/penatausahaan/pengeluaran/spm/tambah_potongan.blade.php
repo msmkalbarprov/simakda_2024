c1ebd13763f31dafc2e9beb8328453e44593fed3
daniels9028 <daniels9028@student.untan.ac.id>
2023-01-31 09:34:26 +0700

    potongan spm

--- resources/views/penatausahaan/pengeluaran/spm/tambah_potongan.blade.php
+++ resources/views/penatausahaan/pengeluaran/spm/tambah_potongan.blade.php
@@ -288,7 +288,7 @@
                                     <option value="2020">2020</option>
                                     <option value="2021">2021</option>
                                     <option value="2022">2022</option>
-                                    <option value="2023" selected>202</option>
+                                    <option value="2023" selected>2023</option>
                                 </optgroup>
                             </select>
                             @error('tahun_pajak')
