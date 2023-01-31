c82d9feb3727c223a36b0dc5aba76283b9ff840b
daniels9028 <daniels9028@student.untan.ac.id>
2023-01-31 09:33:47 +0700

    tahun pajak input pajak potongan spm

--- resources/views/penatausahaan/pengeluaran/spm/tambah_potongan.blade.php
+++ resources/views/penatausahaan/pengeluaran/spm/tambah_potongan.blade.php
@@ -287,7 +287,8 @@
                                     <option value="2019">2019</option>
                                     <option value="2020">2020</option>
                                     <option value="2021">2021</option>
-                                    <option value="2022" selected>2022</option>
+                                    <option value="2022">2022</option>
+                                    <option value="2023" selected>202</option>
                                 </optgroup>
                             </select>
                             @error('tahun_pajak')
