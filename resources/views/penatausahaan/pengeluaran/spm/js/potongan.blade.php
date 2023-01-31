8c8becf967364792f60ccad3775956c01f8f4a15
daniels9028 <daniels9028@student.untan.ac.id>
2023-01-31 10:57:34 +0700

    cek rekening

--- resources/views/penatausahaan/pengeluaran/spm/js/potongan.blade.php
+++ resources/views/penatausahaan/pengeluaran/spm/js/potongan.blade.php
@@ -456,8 +456,10 @@
             if (rekening_potongan == '210105010001' || rekening_potongan == '210105020001' ||
                 rekening_potongan == '210105030001' || rekening_potongan == '210109010001' ||
                 rekening_potongan == '210105040001' || rekening_potongan == '210106010001') {
-                alert('ID Billing wajib diisi!');
-                return;
+                if (!id_billing) {
+                    alert('ID Billing wajib diisi!');
+                    return;
+                }
             }
             let tampungan = tabel_pot.rows().data().toArray().map((value) => {
                 let result = {
