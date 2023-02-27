06a30793a0c0037522cf13060ded1c9b04c59de5
msmkalbarprov <msmkalbar.prov@gmail.com>
2023-02-27 13:34:53 +0700

    update bold list upload csv

--- resources/views/skpd/upload_cms/index.blade.php
+++ resources/views/skpd/upload_cms/index.blade.php
@@ -229,7 +229,7 @@
                 <h2 class="accordion-header" id="headingTwo">
                     <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                         data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                        List Data Upload
+                        <b>List Data Upload csv</b>
                     </button>
                 </h2>
                 <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo"
