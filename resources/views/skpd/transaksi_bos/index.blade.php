0edfc21987df4434841b59a0d924bcca3a0db726
daniels9028 <daniels9028@student.untan.ac.id>
2023-03-06 09:07:55 +0700

    input transaksi bos

---
+++ resources/views/skpd/transaksi_bos/index.blade.php
@@ -0,0 +1,37 @@
+@extends('template.app')
+@section('title', 'Transaksi BOS | SIMAKDA')
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    List Data Transaksi BOS
+                    <a href="{{ route('transaksi_bos.create') }}" class="btn btn-primary" style="float: right;">Tambah</a>
+                </div>
+                <div class="card-body">
+                    <div class="table-rep-plugin">
+                        <div class="table-responsive mb-0" data-pattern="priority-columns">
+                            <table id="transaksi_bos" class="table" style="width: 100%">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 25px;text-align:center">No.</th>
+                                        <th style="width: 50px;text-align:center">Nomor Bukti</th>
+                                        <th style="width: 50px;text-align:center">Tanggal</th>
+                                        <th style="width: 50px;text-align:center">SKPD</th>
+                                        <th style="width: 50px;text-align:center">Keterangan</th>
+                                        <th style="width: 50px;text-align:center">SP2B</th>
+                                        <th style="width: 50px;text-align:center">SPJ</th>
+                                        <th style="width: 200px;text-align:center">Aksi</th>
+                                    </tr>
+                                </thead>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('js')
+    @include('skpd.transaksi_bos.js.index')
+@endsection
