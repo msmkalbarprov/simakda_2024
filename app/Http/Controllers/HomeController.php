d0118d49cd8dcd3b2688771d7baaacf75652f920
msmkalbarprov <msmkalbar.prov@gmail.com>
2023-03-15 22:25:46 +0700

    update home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -97,16 +97,16 @@ class HomeController extends Controller
                     ->first(),
 
                 'data_penagihan' => DB::table('trhtagih')
-                    ->select(DB::raw("isnull(sum(total),0) as penagihan"))
+                    ->select(DB::raw("isnull(sum(total),0) as penagihan,count(*) as totaltagih"))
                     ->where('kd_skpd', $kd_skpd)
                     ->first(),
                 'data_spp' => DB::table('trhspp')
-                    ->select(DB::raw("isnull(sum(nilai),0) as spp"))
+                    ->select(DB::raw("isnull(sum(nilai),0) as spp,count(*) as total"))
                     ->where('kd_skpd', $kd_skpd)
                     ->whereRaw("sp2d_batal is null OR sp2d_batal <> 1")
                     ->first(),
                 'data_spm' => DB::table('trhspm as a')
-                    ->select(DB::raw("isnull(sum(b.nilai),0) as spm"))
+                    ->select(DB::raw("isnull(sum(b.nilai),0) as spm,count(*) as total"))
                     ->join('trhspp as b', function ($join) {
                         $join->on('a.no_spp', '=', 'b.no_spp');
                         $join->on('a.kd_skpd', '=', 'b.kd_skpd');
@@ -115,7 +115,7 @@ class HomeController extends Controller
                     ->where('a.kd_skpd', $kd_skpd)
                     ->first(),
                 'data_sp2d' => DB::table('trhsp2d')
-                    ->select(DB::raw("isnull(sum(nilai),0) as sp2d"))
+                    ->select(DB::raw("isnull(sum(nilai),0) as sp2d,count(*) as total"))
                     ->whereRaw("(sp2d_batal is null OR sp2d_batal <> 1)")
                     ->where('kd_skpd', $kd_skpd)
                     ->first()
