8cd836164a20dfc049d367b2d57f4f6fe2cbc2f7
daniels9028 <daniels9028@student.untan.ac.id>
2023-03-17 15:43:46 +0700

    kelengkapan sp2d

--- resources/views/penatausahaan/pengeluaran/sp2d/cetak/kelengkapan.blade.php
+++ resources/views/penatausahaan/pengeluaran/sp2d/cetak/kelengkapan.blade.php
@@ -5,8 +5,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>KELENGKAPAN SP2D</title>
     <style>
+        table {
+            width: 100%
+        }
+
+        table,
+        td,
+        th {
+            border-collapse: collapse;
+            font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+            vertical-align: top
+        }
+
         .a1 {
             padding-left: 20px;
         }
@@ -18,11 +30,16 @@
 </head>
 
 <body>
-    <div style="text-align: center;margin-top:20px">
-        <h5 style="margin: 2px 0px"><strong>LEMBAR VERIFIKASI KELENGKAPAN DOKUMEN PENERBITAN DANA (SP2D-UP)</strong></h5>
-        <div style="clear: both"></div>
-    </div>
-    <table style="width: 100%">
+    <table>
+        <tr>
+            <td style="text-align: center"><b>LEMBAR VERIFIKASI KELENGKAPAN DOKUMEN PENERBITAN DANA (SP2D-UP)</b></td>
+        </tr>
+    </table>
+
+    <br>
+    <br>
+
+    <table>
         <tbody>
             <tr>
                 <td>SKPD/SATEKER</td>
@@ -48,7 +65,7 @@
                 </td>
             </tr>
             <tr>
-                <td class="a1">1. Nomor dan Tanggal
+                <td class="a1" style="width: 40%">1. Nomor dan Tanggal
                     @if (in_array($beban, ['1', '4']))
                         SPM
                     @elseif ($beban == '2')
@@ -63,7 +80,7 @@
                 <td>{{ $sp2d->no_spm }} dan {{ tanggal($sp2d->tgl_spm) }}</td>
             </tr>
             <tr>
-                <td class="a1">2. Tanggal Terima
+                <td class="a1" style="width: 40%">2. Tanggal Terima
                     @if (in_array($beban, ['1', '4']))
                         SPM
                     @elseif ($beban == '2')
@@ -82,7 +99,8 @@
             </tr>
         </tbody>
     </table>
-    <table style="width: 100%" style="border:1px solid black">
+
+    <table>
         <thead>
             <th></th>
             <th style="font-weight: normal">Ada</th>
@@ -92,48 +110,55 @@
             @include('penatausahaan.pengeluaran.sp2d.cetak.kelengkapan_persyaratan')
         </tbody>
     </table>
-    <table style="margin-top: 20px">
+
+    <br>
+    <br>
+
+    <table>
+        <tbody>
+            <tr>
+                <td class="a1" style="width: 40%">3. Tanggal Pengembalian SPM</td>
+                <td>:</td>
+                <td>........................................................................</td>
+            </tr>
+            <tr>
+                <td class="a1" style="width: 40%">4. Tanggal Terima Kembali SPM</td>
+                <td>:</td>
+                <td>........................................................................</td>
+            </tr>
+        </tbody>
+    </table>
+
+    <br><br><br>
+
+    <table class="table" style="width: 100%">
         <tr>
-            <td class="a1">3. Tanggal Pengembalian SPM</td>
-            <td>:</td>
-            <td>........................................................................</td>
+            <td style="text-align: center">Diperiksa dan diteruskan oleh</td>
+            <td style="margin: 2px 0px;text-align: center">
+                Diteliti dan dikerjakan oleh
+            </td>
         </tr>
         <tr>
-            <td class="a1">4. Tanggal Terima Kembali SPM</td>
-            <td>:</td>
-            <td>........................................................................</td>
+            <td style="padding-bottom: 50px;text-align: center">
+                {{ $ttd1->jabatan }}
+            </td>
+            <td style="padding-bottom: 50px;text-align: center">
+                {{ $ttd2->jabatan }}
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center">
+                <u><b>{{ $ttd1->nama }}</b></u> <br>
+                {{ $ttd1->pangkat }} <br>
+                NIP. {{ $ttd1->nip }}
+            </td>
+            <td style="text-align: center">
+                <u><b>{{ $ttd2->nama }}</b></u> <br>
+                {{ $ttd2->pangkat }} <br>
+                NIP. {{ $ttd2->nip }}
+            </td>
         </tr>
     </table>
-    <div style="padding-top:20px">
-        <table class="table" style="width: 100%">
-            <tr>
-                <td style="text-align: center">Diperiksa dan diteruskan oleh</td>
-                <td style="margin: 2px 0px;text-align: center">
-                    Diteliti dan dikerjakan oleh
-                </td>
-            </tr>
-            <tr>
-                <td style="padding-bottom: 50px;text-align: center">
-                    {{ $ttd1->jabatan }}
-                </td>
-                <td style="padding-bottom: 50px;text-align: center">
-                    {{ $ttd2->jabatan }}
-                </td>
-            </tr>
-            <tr>
-                <td style="text-align: center"><b>{{ $ttd1->nama }}</b></td>
-                <td style="text-align: center"><b>{{ $ttd2->nama }}</b></td>
-            </tr>
-            <tr>
-                <td style="text-align: center">{{ $ttd1->pangkat }}</td>
-                <td style="text-align: center">{{ $ttd2->pangkat }}</td>
-            </tr>
-            <tr>
-                <td style="text-align: center">NIP. {{ $ttd1->nip }}</td>
-                <td style="text-align: center">NIP. {{ $ttd2->nip }}</td>
-            </tr>
-        </table>
-    </div>
 </body>
 
 </html>
