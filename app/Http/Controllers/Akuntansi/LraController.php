dbbf7e161faddb9c2d3567a8a6965794deb690f9
Tetsuyaoy <91108384+msmkalbarprov@users.noreply.github.com>
2023-03-20 11:13:11 +0700

    Update LraController.php

--- app/Http/Controllers/Akuntansi/LraController.php
+++ app/Http/Controllers/Akuntansi/LraController.php
@@ -31,7 +31,7 @@ class LraController extends Controller
         $jenis_data     = $request->jenis_data;
         $jns_rincian    = $request->jns_rincian;
         $skpdunit    = $request->skpdunit;
-        dd($skpdunit);
+        // dd($skpdunit);
         if($request->kd_skpd==''){
             $kd_skpd        = Auth::user()->kd_skpd;
             $skpd_clause="";
