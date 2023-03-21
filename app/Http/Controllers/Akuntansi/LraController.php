8ac48f75b2b94652700a455b5ab7f71dad5a0b77
Tetsuyaoy <91108384+msmkalbarprov@users.noreply.github.com>
2023-03-21 18:12:22 +0700

    create lpe

--- app/Http/Controllers/Akuntansi/LraController.php
+++ app/Http/Controllers/Akuntansi/LraController.php
@@ -1038,4 +1038,163 @@ class LraController extends Controller
             return $view;
         }
     }
+
+    public function cetaklpe(Request $request){
+        ini_set('memory_limit', -1);
+        ini_set('max_execution_time', -1);
+        $bulan          = $request->bulan;
+        $enter          = $request->spasi;
+        $cetak          = $request->cetak;
+        $kd_skpd        = $request->kd_skpd;
+        $skpdunit    = $request->skpdunit;
+        if($request->kd_skpd==''){
+            $kd_skpd        = "";
+            $skpd_clause    = "";
+            $skpd_clauses    = "";
+        }else{
+            if ($skpdunit=="unit") {
+                $kd_skpd=$kd_skpd;
+            }else if ($skpdunit=="skpd") {
+                $kd_skpd=substr($kd_skpd,0,17);
+            }
+            $skpd_clause    = "where left(kd_unit,len('$kd_skpd'))='$kd_skpd'";
+            $skpd_clauses    = "and left(b.kd_skpd,len('$kd_skpd'))='$kd_skpd'";
+        }
+        
+        $thn_ang    = tahun_anggaran();
+        $thn_ang1   = $thn_ang-1;
+
+        $modtahun= $thn_ang%4;
+        
+            if ($modtahun = 0){
+                $nilaibulan=".31 JANUARI.29 FEBRUARI.31 MARET.30 APRIL.31 MEI.30 JUNI.31 JULI.31 AGUSTUS.30 SEPTEMBER.31 OKTOBER.30 NOVEMBER.31 DESEMBER";
+            }else {
+                $nilaibulan=".31 JANUARI.28 FEBRUARI.31 MARET.30 APRIL.31 MEI.30 JUNI.31 JULI.31 AGUSTUS.30 SEPTEMBER.31 OKTOBER.30 NOVEMBER.31 DESEMBER";
+            }
+         
+         $arraybulan=explode(".",$nilaibulan);
+         $nm_bln = $arraybulan[$bulan];
+
+        
+
+
+        $ekuitas_awal = collect(DB::select("SELECT sum(nilai) nilai,sum(nilai_lalu) nilai_lalu
+                        from(
+                        --1 ekuitas_awal
+                        select isnull(sum(nilai),0)nilai,0 nilai_lalu from data_ekuitas_lalu_oyoy($bulan,$thn_ang,$thn_ang1) $skpd_clause
+                        union all
+                        --1 ekuitas lalu
+                        select 0 nilai, isnull(sum(nilai),0)nilai_lalu from data_real_ekuitas_lalu_oyoy($bulan,$thn_ang,$thn_ang1) $skpd_clause
+                        )a"))->first();
+        // dd($ekuitas_awal);
+        $surdef = collect(DB::select("SELECT sum(nilai)nilai,sum(nilai_lalu)nilai_lalu
+                        from(
+                        --2 surplus lo
+                        select sum(nilai_pen-nilai_bel) nilai,0 nilai_lalu
+                        from(
+                            select sum(kredit-debet) as nilai_pen,0 nilai_bel from trdju a inner join trhju b on a.no_voucher=b.no_voucher and a.kd_unit=b.kd_skpd 
+                            where year(tgl_voucher)=$thn_ang and month(tgl_voucher)<=$bulan and left(kd_rek6,1) in ('7') $skpd_clauses
+                            union all
+                            select 0 nilai_pen,sum(debet-kredit) as nilai_bel from trdju a inner join trhju b on a.no_voucher=b.no_voucher and a.kd_unit=b.kd_skpd 
+                            where year(tgl_voucher)=$thn_ang and month(tgl_voucher)<=$bulan and left(kd_rek6,1) in ('8') $skpd_clauses
+                            )a
+                            union all
+                            -- 2 surplus lo lalu
+                            select 0 nilai,isnull(sum(nilai_pen-nilai_bel),0) nilai_lalu
+                            from(
+                            select sum(kredit-debet) as nilai_pen,0 nilai_bel 
+                            from trdju a inner join trhju b on a.no_voucher=b.no_voucher and a.kd_unit=b.kd_skpd 
+                            where year(tgl_voucher)=$thn_ang1 and left(kd_rek6,1) in ('7') $skpd_clauses
+                            union all
+                            select 0 nilai_pen,sum(debet-kredit) as nilai_bel 
+                            from trdju a inner join trhju b on a.no_voucher=b.no_voucher and a.kd_unit=b.kd_skpd 
+                            where year(tgl_voucher)=$thn_ang1 and left(kd_rek6,1) in ('8') $skpd_clauses
+                            )a
+                        )a"))->first();
+
+        $koreksi = collect(DB::select("SELECT sum(nilai)nilai,sum(nilai_lalu)nilai_lalu
+                        from(
+                            --5 nilai lpe 1
+                            select isnull(sum(kredit-debet),0) nilai , 0 nilai_lalu
+                            from trdju a inner join trhju b on a.no_voucher=b.no_voucher and b.kd_skpd=a.kd_unit 
+                            where  reev='2' and kd_rek6='310101010001' and year(b.tgl_voucher)=$thn_ang and month(b.tgl_voucher)<=$bulan $skpd_clauses
+                            union all
+                            --5 nilai lpe 1 lalu
+                            select 0 nilai,isnull(sum(kredit-debet),0) nilai_lalu 
+                            from trdju a inner join trhju b on a.no_voucher=b.no_voucher and b.kd_skpd=a.kd_unit 
+                            where  reev='2' and kd_rek6='310101010001' and year(b.tgl_voucher)=$thn_ang1 $skpd_clauses
+                        )a"))->first();
+
+        $selisih = collect(DB::select("SELECT sum(nilai)nilai,sum(nilai_lalu)nilai_lalu
+                        from(
+                            --6 nilai dr
+                            select isnull(sum(kredit-debet),0) nilai, 0 nilai_lalu 
+                            from trdju a inner join trhju b on a.no_voucher=b.no_voucher and b.kd_skpd=a.kd_unit 
+                            where  reev='1' and kd_rek6='310101010001' and year(tgl_voucher)=$thn_ang and month(tgl_voucher)<=$bulan
+                            union all
+                            --6 nilai dr lalu
+                            select 0 nilai, isnull(sum(kredit-debet),0) nilai_lalu 
+                            from trhju a inner join trdju b on a.no_voucher=b.no_voucher and a.kd_skpd=b.kd_unit 
+                            where  reev='1' and kd_rek6='310101010001' and year(a.tgl_voucher)=$thn_ang1
+                        )a"))->first();
+
+        $lain = collect(DB::select("SELECT sum(nilai)nilai,sum(nilai_lalu)nilai_lalu
+                        from(
+                            --7 nilai lpe2
+                            select isnull(sum(kredit-debet),0) nilai, 0 nilai_lalu 
+                            from trdju a inner join trhju b on a.no_voucher=b.no_voucher and b.kd_skpd=a.kd_unit 
+                            where  reev='3' and kd_rek6='310101010001' and year(tgl_voucher)=$thn_ang and month(tgl_voucher)<=$bulan $skpd_clauses
+                            union all
+                            --7 nilai lpe2 lalu
+                            select 0 nilai,isnull(sum(kredit-debet),0) nilai_lalu 
+                            from trdju a inner join trhju b on a.no_voucher=b.no_voucher and b.kd_skpd=a.kd_unit 
+                            where  reev='3' and kd_rek6='310101010001' and year(tgl_voucher)=$thn_ang1 $skpd_clauses
+                        )a"))->first();
+
+        
+                        
+        
+
+
+        $daerah = DB::table('sclient')->select('daerah')->where('kd_skpd', $kd_skpd)->first();
+            // dd($sus);
+        
+            $data = [
+            'header'            => DB::table('config_app')->select('nm_pemda', 'nm_badan', 'logo_pemda_hp')->first(),
+            // 'ekuitas_awal'      => $ekuitas_awal,
+            'ekuitas_awal'      => $ekuitas_awal->nilai,
+            'ekuitas_awal_lalu'      => $ekuitas_awal->nilai_lalu,
+            'surdef'            => $surdef->nilai,
+            'surdef_lalu'            => $surdef->nilai_lalu,
+            'koreksi'           => $koreksi->nilai,
+            'koreksi_lalu'           => $koreksi->nilai_lalu,
+            'selisih'           => $selisih->nilai,
+            'selisih_lalu'           => $selisih->nilai_lalu,
+            'lain'              => $lain->nilai,
+            'lain_lalu'              => $lain->nilai_lalu,
+            'enter'             => $enter,
+            'daerah'            => $daerah,
+            'bulan'             => $bulan,
+            'kd_skpd'           => $kd_skpd,
+            'nm_bln'            => $nm_bln,
+            'thn_ang'           => $thn_ang,
+            'thn_ang1'         => $thn_ang1         
+            ];
+        // dd($data['ekuitas_awal']->nilai);
+            $view =  view('akuntansi.cetakan.lpe')->with($data);
+
+        
+        if ($cetak == '1') {
+            return $view;
+        } else if ($cetak == '2') {
+            $pdf = PDF::loadHtml($view)->setPaper('legal');
+            return $pdf->stream('LPE.pdf');
+        } else {
+
+            header("Cache-Control: no-cache, no-store, must_revalidate");
+            header('Content-Type: application/vnd.ms-excel');
+            header('Content-Disposition: attachement; filename="LPE.xls"');
+            return $view;
+        }
+    }
 }
--- resources/views/akuntansi/modal/lrasemester.blade.php
+++ resources/views/akuntansi/modal/lrasemester.blade.php
@@ -392,3 +392,95 @@
         </div>
     </div>
 </div>
+
+{{-- lpe --}}
+<div id="modal_cetak_lpe" class="modal" role="dialog" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title"><label for="labelcetak_semester" id="labelcetak_semester"></label></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                {{-- Pilihan SKPD/Unit --}}
+                <div class="mb-3 row">
+                    <div class="col-md-6">
+                        <label for="kd_skpd" class="form-label">Pilih</label><br>
+                        <div class=" form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="pilihan_lpe" id="pilihan0"
+                                value="keseluruhan">
+                            <label class="form-check-label" for="pilihan">Keseluruhan</label>
+                        </div>
+                        <div class=" form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="pilihan_lpe" id="pilihan1"
+                                value="skpd">
+                            <label class="form-check-label" for="pilihan">SKPD</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="pilihan_lpe" id="pilihan2"
+                                value="unit">
+                            <label class="form-check-label" for="pilihan">Unit</label>
+                        </div>
+                    </div>
+                    {{-- Bulan --}}
+                    <div class="col-md-6" >
+                        <div id="baris_skpd_lpe">
+                            <label for="kd_skpd_lpe" class="form-label">Kode SKPD</label>
+                            <select class="form-control select_lpe  @error('kd_skpd') is-invalid @enderror"
+                                style=" width: 100%;" id="kd_skpd_lpe" name="kd_skpd_lpe">
+                                <option value="" disabled selected>Silahkan Pilih</option>
+                            </select>
+                            @error('kd_skpd_lpe')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                            @enderror
+                        </div>
+                        
+                    </div>
+                </div>
+
+                <div class="mb-3 row">
+                    
+                    {{-- PERIODE /BULAN --}}
+                        
+
+                        <div id="baris_bulan" class="col-md-6">
+                            <label for="bulan_lpe" class="form-label">Bulan</label>
+                            <select name="bulan_lpe" class="form-control select_lpe" id="bulan_lpe">
+                                <option value="">Silahkan Pilih</option>
+                                <option value="1">Januari</option>
+                                <option value="2">Februari</option>
+                                <option value="3">Maret</option>
+                                <option value="4">April</option>
+                                <option value="5">Mei</option>
+                                <option value="6">Juni</option>
+                                <option value="7">Juli</option>
+                                <option value="8">Agustus</option>
+                                <option value="9">September</option>
+                                <option value="10">Oktober</option>
+                                <option value="11">November</option>
+                                <option value="12">Desember</option>
+                            </select>
+                        </div>
+                        
+                </div>
+
+                
+
+                
+
+                <div class="mb-3 row">
+                    <div class="col-md-12 text-center">
+                        <button type="button" class="btn btn-danger btn-md bku_pdf" data-jenis="pdf"
+                            name="bku_pdf"> PDF</button>
+                        <button type="button" class="btn btn-dark btn-md bku_layar" data-jenis="layar"
+                            name="bku_layar">Layar</button>
+                        <button type="button" class="btn btn-success btn-md bku_excel" data-jenis="excel"
+                            name="bku_excel">Excel</button>
+                        <button type="button" class="btn btn-md btn-secondary"
+                            data-bs-dismiss="modal">Tutup</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
