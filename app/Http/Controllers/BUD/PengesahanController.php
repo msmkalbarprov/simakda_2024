7bdbccee0d9bcbdb07c452079bfcf5ee014c45a7
daniels9028 <daniels9028@student.untan.ac.id>
2023-03-29 11:32:14 +0700

    cetakan pengesahan lpj up gu

--- app/Http/Controllers/BUD/PengesahanController.php
+++ app/Http/Controllers/BUD/PengesahanController.php
@@ -152,6 +152,10 @@ class PengesahanController extends Controller
         $kd_sub_kegiatan = $request->kd_sub_kegiatan;
         $pilihan = $request->pilihan;
         $jenis_print = $request->jenis_print;
+        $atas = $request->atas;
+        $bawah = $request->bawah;
+        $kiri = $request->kiri;
+        $kanan = $request->kanan;
         $status_anggaran = status_anggaran();
 
         if ($pilihan == '0') {
@@ -173,10 +177,10 @@ class PengesahanController extends Controller
                                 GROUP BY LEFT(a.kd_sub_kegiatan,12), b.nm_kegiatan
 
                                 UNION ALL
-                                
+
                                 SELECT 2 as urut, a.kd_sub_kegiatan as kode, b.nm_sub_kegiatan as uraian, SUM(a.nilai) as nilai
-                                FROM trlpj a INNER JOIN trdtransout b ON a.kd_sub_kegiatan=b.kd_sub_kegiatan 
-AND a.kd_skpd=b.kd_skpd and a.kd_rek6=b.kd_rek6 and a.no_bukti=b.no_bukti
+                                FROM trlpj a INNER JOIN trdtransout b ON a.kd_sub_kegiatan=b.kd_sub_kegiatan
+                                AND a.kd_skpd=b.kd_skpd and a.kd_rek6=b.kd_rek6 and a.no_bukti=b.no_bukti
                                 WHERE no_lpj=? AND a.kd_bp_skpd=?
                                 AND a.no_bukti IN (SELECT no_bukti FROM trhtransout WHERE left(kd_skpd,17)=left(?,17)
                                 --AND (panjar NOT IN ('3') or panjar IS NULL)
@@ -359,8 +363,12 @@ AND a.kd_skpd=b.kd_skpd and a.kd_rek6=b.kd_rek6 and a.no_bukti=b.no_bukti
         if ($jenis_print == 'pdf') {
             $pdf = PDF::loadHtml($view)
                 ->setPaper('legal')
-                ->setOption('margin-left', 15)
-                ->setOption('margin-right', 15);
+                ->setOption('page-width', 215)
+                ->setOption('page-height', 330)
+                ->setOption('margin-top', $atas)
+                ->setOption('margin-bottom', $bawah)
+                ->setOption('margin-left', $kiri)
+                ->setOption('margin-right', $kanan);
             return $pdf->stream('laporan.pdf');
         } else {
             return $view;
