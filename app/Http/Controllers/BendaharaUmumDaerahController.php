5ae44ef8b2eea2598b649a286e49c325b5e00d93
daniels9028 <daniels9028@student.untan.ac.id>
2023-05-08 13:21:36 +0700

    rekap gaji 2023

--- app/Http/Controllers/BendaharaUmumDaerahController.php
+++ app/Http/Controllers/BendaharaUmumDaerahController.php
@@ -2243,7 +2243,8 @@ class BendaharaUmumDaerahController extends Controller
             })
             ->where(function ($query) use ($req) {
                 if ($req['pilihan'] == '12' || $req['pilihan'] == '22') {
-                    $query->where(DB::raw("MONTH(tgl_sp2d)"), $req['bulan'])->where('a.jenis_beban', '1');
+                    // $query->where(DB::raw("MONTH(tgl_sp2d)"), $req['bulan'])->where('a.jenis_beban', '1');
+                    $query->where(DB::raw("MONTH(tgl_sp2d)"), $req['bulan']);
                 }
                 if ($req['pilihan'] == '13' || $req['pilihan'] == '23') {
                     $query->whereBetween('tgl_sp2d', [$req['periode1'], $req['periode2']]);
