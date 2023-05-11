5f109de9a54ffec07f534b5d8927fc90b5703f4c
daniels9028 <daniels9028@student.untan.ac.id>
2023-05-11 15:23:28 +0700

    kelengkapan spm

--- resources/views/penatausahaan/pengeluaran/spm/cetak/kelengkapan.blade.php
+++ resources/views/penatausahaan/pengeluaran/spm/cetak/kelengkapan.blade.php
@@ -2823,17 +2823,20 @@
     <table style="width: 100%;font-family:'Open Sans', Helvetica,Arial,sans-serif" class="rincian">
         <tbody>
             <tr>
-                <td style="text-align: center;padding-left:600px">
+                <td style="width: 50%"></td>
+                <td style="text-align: center">
                     Dikerjakan oleh
                 </td>
             </tr>
             <tr>
-                <td style="padding-bottom: 50px;text-align: center;padding-left:600px">
+                <td style="width: 50%"></td>
+                <td style="padding-bottom: 50px;text-align: center">
                     {{ $pptk->jabatan }}
                 </td>
             </tr>
             <tr>
-                <td style="text-align: center;padding-left:600px">
+                <td style="width: 50%"></td>
+                <td style="text-align: center">
                     <b><u>{{ $pptk->nama }}</u></b>
                     <br>
                     {{ $pptk->pangkat }}
@@ -2841,12 +2844,6 @@
                     NIP. {{ $pptk->nip }}
                 </td>
             </tr>
-            {{-- <tr>
-                <td style="text-align: center;padding-left:600px">{{ $pptk->pangkat }}</td>
-            </tr> --}}
-            {{-- <tr>
-                <td style="text-align: center;padding-left:600px">NIP. {{ $pptk->nip }}</td>
-            </tr> --}}
         </tbody>
     </table>
 
