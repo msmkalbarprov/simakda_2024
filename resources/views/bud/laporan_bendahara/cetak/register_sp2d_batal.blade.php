b8ed2cf6ec19d154ac716632b9841e88bf53f95a
daniels9028 <daniels9028@student.untan.ac.id>
2023-05-23 15:07:56 +0700

    tulisan register sp2d batal

--- resources/views/bud/laporan_bendahara/cetak/register_sp2d_batal.blade.php
+++ resources/views/bud/laporan_bendahara/cetak/register_sp2d_batal.blade.php
@@ -44,8 +44,8 @@
 {{-- <body onload="window.print()"> --}}
 
 <body>
-    <table style="border-collapse:collapse;font-family: Open Sans; font-size:14px" width="100%" align="center"
-        border="0" cellspacing="0" cellpadding="0">
+    <table style="border-collapse:collapse;font-family:'Open Sans', Helvetica,Arial,sans-serif; font-size:18px"
+        width="100%" align="center" border="0" cellspacing="0" cellpadding="0">
         <tr>
             <td style="text-align: center">
                 <b>PEMERINTAH
@@ -54,9 +54,9 @@
             </td>
         </tr>
         <tr>
-            <td style="text-align: center">
+            <td style="text-align: center;font-family:'Open Sans', Helvetica,Arial,sans-serif">
                 <b>
-                    DAFTAR SPM DAN SP2D TAHUN ANGGARAN {{ tahun_anggaran() }}
+                    REGISTER SP2D BATAL
                 </b>
             </td>
         </tr>
@@ -64,7 +64,7 @@
 
     <br><br>
 
-    <table style="width: 100%" border="1" id="rincian">
+    <table style="width: 100%;font-family:'Open Sans', Helvetica,Arial,sans-serif" border="1" id="rincian">
         <thead>
             <tr>
                 <th>No.</th>
