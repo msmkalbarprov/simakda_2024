4708737b107a56532ea206fcc6b792bcfaeaf7e2
daniels9028 <daniels9028@student.untan.ac.id>
2023-12-01 11:22:18 +0700

    feat: ringkasan LS (6)

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1734,7 +1734,8 @@ function sp2dbelanja_gu($kd_skpd, $no_spp, $tgl_spp, $beban, $kd_sub_kegiatan)
             ->join('trhspp as a', function ($join) {
                 $join->on('a.no_spp', '=', 'b.no_spp');
                 $join->on('a.kd_skpd', '=', 'b.kd_skpd');
-            })->join('trhsp2d as c', function ($join) {
+            })
+            ->join('trhsp2d as c', function ($join) {
                 $join->on('b.no_spp', '=', 'c.no_spp');
                 $join->on('b.kd_skpd', '=', 'c.kd_skpd');
             })
@@ -1903,13 +1904,23 @@ function sp2dbelanja_barang($kd_skpd, $no_spp, $tgl_spp, $beban, $kd_sub_kegiata
             })->where(['a.jns_spp' => '6', 'a.kd_skpd' => $kd_skpd])->where('a.no_spp', '!=', $no_spp)->where('c.tgl_sp2d', '<=', $tgl_spp)->select(DB::raw("SUM(b.nilai) as nilai"))->first();
         }
     } else if (in_array($beban, ['3', '4'])) {
-        $nilai = DB::table('trdspp as b')->join('trhspp as a', function ($join) {
-            $join->on('a.no_spp', '=', 'b.no_spp');
-            $join->on('a.kd_skpd', '=', 'b.kd_skpd');
-        })->join('trhsp2d as c', function ($join) {
-            $join->on('a.no_spp', '=', 'c.no_spp');
-            $join->on('a.kd_skpd', '=', 'c.kd_skpd');
-        })->where(['a.jns_spp' => '6', 'a.kd_skpd' => $kd_skpd, 'b.kd_sub_kegiatan' => $kd_sub_kegiatan])->where('a.no_spp', '!=', $no_spp)->where('c.tgl_sp2d', '<=', $tgl_spp)->select(DB::raw("SUM(b.nilai) as nilai"))->first();
+        $nilai = DB::table('trdspp as b')
+            ->join('trhspp as a', function ($join) {
+                $join->on('a.no_spp', '=', 'b.no_spp');
+                $join->on('a.kd_skpd', '=', 'b.kd_skpd');
+            })
+            ->join('trhsp2d as c', function ($join) {
+                $join->on('a.no_spp', '=', 'c.no_spp');
+                $join->on('a.kd_skpd', '=', 'c.kd_skpd');
+            })
+            ->where(['a.jns_spp' => '6', 'a.kd_skpd' => $kd_skpd, 'b.kd_sub_kegiatan' => $kd_sub_kegiatan])
+            ->where('a.no_spp', '!=', $no_spp)
+            ->where('c.tgl_sp2d', '<=', $tgl_spp)
+            ->where(function ($query) {
+                $query->where('a.sp2d_batal', '')->orWhereNull('a.sp2d_batal');
+            })
+            ->select(DB::raw("SUM(b.nilai) as nilai"))
+            ->first();
     } elseif ($beban == '6') {
         if (substr($kd_skpd, 18, 4) == '0000') {
             $skpd = "LEFT(a.kd_skpd,17)=LEFT('$kd_skpd',17)";
