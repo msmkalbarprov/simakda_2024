8eaf418b9101370b0ba5e8da68e763999680f983
daniels9028 <daniels9028@student.untan.ac.id>
2024-02-13 18:12:59 +0700

    feat: hapus cookie 2023 jika login 2024

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -6,6 +6,7 @@ use App\Providers\RouteServiceProvider;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 
 class RedirectIfAuthenticated
 {
@@ -24,6 +25,8 @@ class RedirectIfAuthenticated
         foreach ($guards as $guard) {
             if (Auth::guard($guard)->check()) {
                 // return redirect(RouteServiceProvider::HOME);
+                Cookie::queue(Cookie::forget('simakda_2023_session'));
+                Cookie::queue(Cookie::forget('laravel_session'));
                 return redirect()->route('home');
             }
         }
