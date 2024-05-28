b7c1e604f789a2db9fb3077478a3ecce3e32d437
daniels9028 <daniels9028@student.untan.ac.id>
2024-05-28 14:24:45 +0700

    feat sp2d

--- resources/views/penatausahaan/pengeluaran/sp2d/cetak/sp2d.blade.php
+++ resources/views/penatausahaan/pengeluaran/sp2d/cetak/sp2d.blade.php
@@ -442,7 +442,7 @@
                     <td>Pengguna Anggaran/Kuasa Pengguna Anggaran</td>
                     <td></td>
                     <td style="text-align: center" class="kanan">
-                        <b>{{ $ttd1->jabatan1 }}</b>
+                        <b>{{ $ttd1->jabatan }}</b>
                     </td>
                 </tr>
                 <tr>
